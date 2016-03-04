847ed504d7d535b280486576836fff8fa2683b36
Filipe Silva <filipesilva.dev@gmail.com>
2016-03-04 16:57:58 -0300

    rising product gallery

---
+++ js/bora-rachar_scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  //Google API da fonte 'Lato'e 'Montserrat':
+  WebFontConfig = {
+    google: { families: [
+      'Lato:400,300,700,100:latin',
+      'Montserrat:400,700:latin'
+      ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+
+(function getFilterValue () {
+   // body...  
+})
+
+});
